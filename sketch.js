b67f492e7330b981ad949fa88d8cc837723d192a
usung325 <usung325@gmail.com>
2024-02-09 23:47:33 -0500

    made it bounce

--- sketch.js
+++ sketch.js
@@ -1,14 +1,37 @@
 let img;
+let mx = 1324 - 600
+let my = 1824 - 920;
+let squareSize = 200;
+let squareSizeY = 100;
+
+let dvdX = 0;
+let dvdY = 0;
+let xSpeed = 1;
+let ySpeed = 1;
 
 function preload() {
   img = loadImage('poster1.png');
-}
-
+  imTxt = loadImage('text.png');
+};
 function setup() {
-  let mx = 1324/2.01
-  let my = 1824/2.01
-  createCanvas(mx,my)
+  createCanvas(mx,my);
+};
+function draw(){
   image(img, 0, 0, mx, my);
-  // img.resize(1920, 1080);
-  describe('Image of the underside of a white umbrella and a gridded ceiling.');
-}
+  image(imTxt, dvdX, dvdY, squareSize, squareSizeY )
+  fill(0);
+  // rect(dvdX,dvdY,squareSize, squareSize);
+
+  dvdX = dvdX + xSpeed;
+  dvdY = dvdY + ySpeed;
+
+  if (dvdX + squareSize == mx || dvdX == 0) {
+    xSpeed = xSpeed * -1
+  }
+
+  if (dvdY + squareSizeY == my || dvdY == 0) {
+    ySpeed = ySpeed * -1
+  }
+
+
+};
